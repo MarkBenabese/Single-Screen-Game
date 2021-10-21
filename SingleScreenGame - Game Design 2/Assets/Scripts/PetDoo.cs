48e5770dfe7777ce6fedd299e2720db9182f374d
TranquilSean <C20740015@mytudublin.ie>
2021-10-21 13:46:08 +0100

    Playtest2.0(edits)

--- SingleScreenGame - Game Design 2/Assets/Scripts/PetDoo.cs
+++ SingleScreenGame - Game Design 2/Assets/Scripts/PetDoo.cs
@@ -1,10 +1,8 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class PlayerDamage : MonoBehaviour
+public class PetDoo : MonoBehaviour
 {
     [SerializeField] private Collider playerCheck;
     [SerializeField] private LayerMask playerLayers;
@@ -14,19 +12,10 @@ public class PlayerDamage : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.tag == "Enemy")
-        {
-            
-            transform.position = new Vector3(30.56f,1.51f,-42.38f);
-
-            
-            manager.AddScore(-scoreToGive);
-
-        }
         if (other.gameObject.tag == "NPC")
         {
 
-            //transform.position = new Vector3(22.8916f, 4.205579f, -42.29498f);
+            //transform.position = new Vector3(22.89161f, 4.205579f, -42.29498f);
 
 
             manager.AddScore(scoreToGive);
