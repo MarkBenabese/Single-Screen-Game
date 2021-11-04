431d967f4c2ded7b3cf55070ee525324eb4419a9
Tearai <77899947+Tearai@users.noreply.github.com>
2021-11-04 19:14:41 +0000

    Revert "bruh"
    This reverts commit 6c9c9e41c7005b1282eae783c517c1c3dc9bbff4, reversing
    changes made to 0288839c997aca3d715fadf711f6a39520b6ef78.

--- SingleScreenGame - Game Design 2/Assets/Scripts/GameManager.cs
+++ SingleScreenGame - Game Design 2/Assets/Scripts/GameManager.cs
@@ -14,9 +14,9 @@ public class GameManager : MonoBehaviour
     
     private void Start()
     {
-        currentEmotion = 30;
+        currentEmotion = 25;
         emotionBar.SetEmotion(currentEmotion);
-        PauseGame();
+        //PauseGame();
     }
 
     public void PauseGame()
